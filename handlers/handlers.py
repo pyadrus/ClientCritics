42c5ec692e62d10c0a517bb7415829c1499c6b52
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-07-09 11:40:27 +0300

    Структурирование проекта

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -5,6 +5,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from loguru import logger
 from dispatcher import dp
+from keyboards.keyboards import start_keyboard
 
 
 @dp.message(CommandStart())
@@ -13,7 +14,10 @@ async def command_start_handler(message: Message) -> None:
     Отвечает на команду /start
     """
     logger.info(f"Пользователь ввел команду /start: {message.from_user.full_name}")
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.answer(
+        text=f"Приветствую, {html.bold(message.from_user.full_name)}!",  # Текст сообщения пользователю
+        reply_markup=start_keyboard()  # Клавиатура
+    )
 
 
 def register_handlers() -> None:
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def start_keyboard() -> InlineKeyboardMarkup:
