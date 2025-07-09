c4d550d66e0c0ce40431cdab9a57471c25380a11
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-07-09 11:15:13 +0300

    Структурирование проекта

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -3,7 +3,7 @@
 from aiogram import html
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
+from loguru import logger
 from dispatcher import dp
 
 
@@ -12,4 +12,10 @@ async def command_start_handler(message: Message) -> None:
     """
     Отвечает на команду /start
     """
+    logger.info(f"Пользователь ввел команду /start: {message.from_user.full_name}")
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+def register_handlers() -> None:
+    """Регистрация хендлера"""
+    dp.message.register(command_start_handler)
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import logging
 import sys
 
 from dispatcher import bot, dp
+from handlers.handlers import register_handlers
 
+# https://docs.aiogram.dev/en/dev-3.x/
 
 async def main() -> None:
-
     # запуск
     await dp.start_polling(bot)
+    register_handlers()
 
 
 if __name__ == "__main__":
